33d51eb303e196a73eee7b4b3cc4e0c89225cee7
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2023-10-07 09:37:12 +0100

    Code 3 abstract factory

---
+++ code3/ProduitA1.java
@@ -0,0 +1,8 @@
+import construction.abstractFactory.ProduitA;
+
+public class ProduitA1 extends ProduitA{
+
+    public void methodeA(){
+        System.out.println("ProduitA1.methodeA()");
+    }
+}
