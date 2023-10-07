a7366e76b00eec2b8454d1ed9ff8010061b5b4ae
leonelmaken <103751176+leonelmaken@users.noreply.github.com>
2023-10-07 18:38:26 +0100

    produit 3 avec abstract factory

---
+++ code3/ProduitFactory3.java
@@ -0,0 +1,8 @@
+public class ProduitFactory3 implements IProduitFactory {
+public ProduitA getProduitA() {
+return new ProduitA3();
+}
+public ProduitB getProduitB() {
+   return new ProduitB3();
+   }
+}
